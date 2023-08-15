f4b2a78baacb82d87de17ceaa5520391ff00649d
tmengwang <tmengwang@tencent.com>
2023-08-15 10:31:35 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,12 +7,12 @@ export default {
         items: [
           {
             text: "面试题",
-            link: "/fe/interview/",
+            // link: "/fe/interview/",
             items: [{ text: "react面试题", link: "/fe/interview/react" }],
           },
           {
             text: "算法",
-            link: "/fe/alg/",
+            // link: "/fe/alg/",
             items: [
               { text: "时间与空间复杂度", link: "/fe/alg/complexity" },
               { text: "栈", link: "/fe/alg/stack" }
