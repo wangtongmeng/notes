d3ae3aaae08601cbbb8aad062b8605abfcfc9f31
wangtongmeng <tongmengwang@163.com>
2023-08-26 22:51:35 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -44,6 +44,14 @@ export default {
               items: [
                 {text: 'TypeScript安装与编译', link: '/fe/typescript/01.TypeScript安装与编译'},
                 {text: '数据类型', link: '/fe/typescript/02.数据类型'},
+                {text: '函数', link: '/fe/typescript/03.函数'},
+                {text: '类', link: '/fe/typescript/04.类'},
+                {text: '接口', link: '/fe/typescript/05.接口'},
+                {text: '泛型', link: '/fe/typescript/06.泛型'},
+                {text: '结构类型系统', link: '/fe/typescript/08.结构类型系统'},
+                {text: '类型变换', link: '/fe/typescript/09.类型变换'},
+                {text: '模块VS命名空间', link: '/fe/typescript/10.模块VS命名空间'},
+                {text: '类型声明', link: '/fe/typescript/11.类型声明'},
               ]
             },
             { text: 'Node', collapsed: true },
